b5d958f655194c5fcd6c1a78e513b3c7ad037c1d
GEONHO HAN <43363413+borish3198@users.noreply.github.com>
2021-10-06 23:13:09 +0900

    Create Sort_2.py

---
+++ 이것이 취업을 위한 코딩 테스트다/Sort_2.py
@@ -0,0 +1,13 @@
+# 삽입정렬을 활용한 오름차순
+
+data = [list(map(str, input().split())) for _ in range(int(input()))]
+def point(i):
+    return int(data[i][1])
+    
+for i in range(1, len(data)):
+    for j in range(i, 0, -1):
+        if point(j)<point(j-1):
+            data[j], data[j-1] = data[j-1], data[j]
+
+for i in data:
+  print(i[1])
