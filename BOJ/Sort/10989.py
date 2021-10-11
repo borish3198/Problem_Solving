42ea086a9241047660053f3cecc6e2e33058f7cb
GEONHO HAN <43363413+borish3198@users.noreply.github.com>
2021-10-11 23:49:48 +0900

    Update 10989.py

--- BOJ/Sort/10989.py
+++ BOJ/Sort/10989.py
@@ -17,5 +17,10 @@ for i in range(10001):
   for j in range(g[i]):
     print(i)
 
-    
+'''
+input()과 가장 큰 차이점은 input() 은 내장 함수로 취급되는 반면 sys 에 속하는 메소드들은 file object로 취급된다. 
 
+즉, 사용자의 입력만을 받는 buffer를 하나 만들어 그 buffer에서 읽어들이는 것이다.
+
+input()은 더 이상 입력이 없는데도 수행될 경우 EOFerror를 뱉어내는 반면 sys.stdin.readline()은 빈 문자열을 반환한다. 어떻게 보면 에러에서도 안전할 듯?
+'''
