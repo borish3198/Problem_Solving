92d05f17327095114ebbc4c8651b538faee97e5e
GEONHO HAN <43363413+borish3198@users.noreply.github.com>
2021-12-23 00:17:10 +0900

    Create 4195_try1.py

---
+++ BOJ/find_union/4195_try1.py
@@ -0,0 +1,38 @@
+n = int(input())
+
+def find(a):
+    if a!=parent[a]:
+        parent[a] = find(parent[a])
+    return parent[a]
+
+def add(a):
+    if a not in list(parent.keys()):
+        parent[a] = a
+        count[a] = 1
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a<b:
+        parent[b]=a
+        count[a]+=count[b]
+    else:
+        parent[a]=b
+        count[b]+=count[a]
+
+for i in range(n):
+    m = int(input())
+    parent = {}
+    count = {}
+    for _ in range(m):
+        a, b = map(str, input().split())
+        add(a)
+        add(b)
+        union(a, b)
+        if a<b:
+            print(count[find(a)])
+        else:
+            print(count[find(b)])
+        
+    
+    
